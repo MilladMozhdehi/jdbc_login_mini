a80deab52191f5be009321e38f08db98a44c6a38
MilladMozhdehi <miladmozhdehipour73@gmail.com>
2024-01-08 20:38:32 +0330

    add constructor Without id

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -17,6 +17,13 @@ public class User {
         this.password = password;
     }
 
+    public User(String firstname, String lastname, String username, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.password = password;
+    }
+
     public int getId() {
         return id;
     }
