f36ba8a7a5aec799b674dd8ebd0b6bb5bdbbf866
MilladMozhdehi <miladmozhdehipour73@gmail.com>
2024-01-08 20:18:30 +0330

    publicMenu() class added to Menu class

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Menu {
+
+    Scanner scanner = new Scanner(System.in);
+    public void publicMenu(){
+        System.out.println("1-SIGN UP");
+        System.out.println("2-SIGN IN");
+        System.out.println("3-EXIT");
+        System.out.print("Choose your number: ");
+
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+    }
 }
